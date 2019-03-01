12ae35d23d5efba8ac4cc34d9cf529247fb8d179
Will Muto <willmuto@Wills-MacBook-Pro.local>
2019-02-28 21:28:09 -0800

    Cleanup; some refactoring and documentation.

--- municlock.ino
+++ municlock.ino
@@ -2,10 +2,15 @@
 #include <Adafruit_GFX.h>
 #include "Adafruit_LEDBackpack.h"
 
+// Contains SSID and PASSWORD for wifi network,
+// both const char* variables.
+#include "mywifi.h"
+
 /*
 
   Setting the 14-segment array
   -------------------------------------
+   https://learn.adafruit.com/14-segment-alpha-numeric-led-featherwing/overview
 
               A
           ----------
@@ -21,14 +26,14 @@
     4000  2000  1000  800  400  200  100  80   40   20  10  8  4  2  1
 
 
-
   Getting the route data for Muni
   -------------------------------------
+   https://gist.github.com/grantland/7cf4097dd9cdf0dfed14#file-readme-md
 
-   Stop id can be found with the following HTTP request:
+   Stop number can be found with the following HTTP request:
    http://webservices.nextbus.com/service/publicXMLFeed?command=routeConfig&a=sf-muni&r=<ROUTE#>
 
-   Once you have a stop id, you can query the prediction with the following:
+   Once you have a stop number, you can query the prediction with the following:
    http://webservices.nextbus.com/service/publicXMLFeed?command=predictions&a=sf-muni&r=<ROUTE#>&s=<STOP#>
   
  */
@@ -44,12 +49,26 @@ unsigned int WIFI_RECONNECT_TIME = 100;       // in milliseconds
 
 const int HTTP_PORT = 80;
 const char* HTTP_URL = "webservices.nextbus.com";
-//const char* SSID = "NETWORKNAME";
-//const char* PASSWORD =  "PASSWORD";
-
-Adafruit_AlphaNum4 alpha4 = Adafruit_AlphaNum4();
 
 int NUM_ROUTES = 6;
+
+Adafruit_AlphaNum4 alpha4 = Adafruit_AlphaNum4();
+WiFiClient client;
+
+// Multi-dimensional array for loading animation.
+// Each row is a frame in the animation, each column
+// maps to a digit on the display.
+unsigned int NUM_LOADING_FRAMES = 5;
+uint16_t loadingAnim[5][4] = {
+  { 0x0000, 0x0000, 0x0000, 0x0000 },  // "    "
+  { 0x0900, 0x0000, 0x0000, 0x0000 },  // ">   "
+  { 0x0900, 0x0900, 0x0000, 0x0000 },  // ">>  "
+  { 0x0900, 0x0900, 0x0900, 0x0000 },  // ">>> "
+  { 0x0900, 0x0900, 0x0900, 0x0900 }   // ">>>>"
+};
+
+// Multi-dimensional array which is ordered:
+//  Route#, Stop#, Direction
 String routesAndStops[6][3] = {
   {"37", "4168", INBOUND}, 
   {"37", "4167", OUTBOUND},
@@ -59,7 +78,14 @@ String routesAndStops[6][3] = {
   {"33", "4080", OUTBOUND}
 };
 
-void setup() {
+// Counter used for loading animation on display
+int waitingIndex;
+
+void setup() 
+{
+  // First time initialization of display 
+  // and wifi client.
+  
   Serial.begin(SERIAL_BAUD);
   
   alpha4.begin(0x70);
@@ -70,48 +96,52 @@ void setup() {
   String mac = WiFi.macAddress();
   if (DEBUG) Serial.println(mac);
 
-  int waitingIndex = 0;
   while (WiFi.status() != WL_CONNECTED) {
     delay(WIFI_RECONNECT_TIME);
-    
-    if (waitingIndex == 4)
-    {
-      waitingIndex = 0;
-    }
-    else 
-    {
-      waitingIndex++;
-    }
-    loading(waitingIndex);
-    
+    updateLoadingAnimation();
   }
 
   debug("Connected to the WiFi network " + String(SSID)); 
 }
 
-void loading(int index) {
-  uint16_t loadingChars[5][4] = {
-    { 0x0000, 0x0000, 0x0000, 0x0000 },
-    { 0x0900, 0x0000, 0x0000, 0x0000 }, 
-    { 0x0900, 0x0900, 0x0000, 0x0000 }, 
-    { 0x0900, 0x0900, 0x0900, 0x0000 }, 
-    { 0x0900, 0x0900, 0x0900, 0x0900 }
-  };
+void updateLoadingAnimation() 
+{
+  // Increment the animation counter and write the
+  // characters for the that frame to the display.
+  
+  if (waitingIndex < NUM_LOADING_FRAMES - 1)
+  {
+    waitingIndex++;
+  }
+  else 
+  {
+    waitingIndex = 0;
+  }
 
   alpha4.clear();
 
-  for (int i = 0; i < 4; i++) {
-    alpha4.writeDigitRaw(i, loadingChars[index][i]);
+  for (int i = 0; i < 4; i++) 
+  {
+    alpha4.writeDigitRaw(i, loadingAnim[waitingIndex][i]);
   }
                        
   alpha4.writeDisplay();
 }
 
-void debug(String msg) {
+void debug(String msg) 
+{
+  // Print debug output.
+  
   if (DEBUG) Serial.println(msg);
 }
 
-void displayWrite(String msg, bool dot) {
+void displayWrite(String msg, bool dot) 
+{
+  // Send a message to the display. Characters after
+  // the fourth will be discarded. Optionally light
+  // up the dot on the second character; used for
+  // specifying bus route direction (inbound vs outbound).
+  
   alpha4.clear();
   alpha4.writeDigitAscii(0, msg[0]);
   alpha4.writeDigitAscii(1, msg[1], dot);
@@ -120,85 +150,112 @@ void displayWrite(String msg, bool dot) {
   alpha4.writeDisplay();
 }
 
-void loop() {
-
-    WiFiClient client;
-
-    // Loop through routes
-    for (int i = 0; i < NUM_ROUTES; i++) {
-
-      debug("Connecting to " + String(HTTP_URL)); 
-      
-      if (!client.connect(HTTP_URL, HTTP_PORT)) {
-        debug("Error connecting to " + String(HTTP_URL));
-        
-        delay(NEXTBUS_RECONNECT_TIME);
-        return;
-      }
-      
-      debug("Connected to " + String(HTTP_URL));
-
-      String routeIndex = routesAndStops[i][0];
-      String stopIndex = routesAndStops[i][1];
-      String routeDir = routesAndStops[i][2];
-
-      bool dot;
-      if (routeDir == INBOUND) dot = false;
-      if (routeDir == OUTBOUND) dot = true;
-
-      String query = String("/service/publicXMLFeed?command=predictions&a=sf-muni&r=" + 
-                            routeIndex + 
-                            "&s=" + 
-                            stopIndex);
-                            
-      String request = String("GET " + query + " HTTP/1.0");
-                              
-      debug("Making request");
-      debug(request);
-      debug(String(HTTP_URL) + query);
-      
-      client.println(request);
-      client.println("Host: " + String(HTTP_URL));
-      client.println("User-Agent: ESP32");
-      client.println();
-
-      String s;
-      char c;
-      if(client.find("<prediction ")){
-        
-        debug("Found prediction");
-        s = client.readStringUntil('>');
+String createRequest(String routeNum, String stopNum) 
+{
+  // Create the query string that is sent to the HTTP client
+  // in the form of "GET /path/to/endpoint HTTP/1.0"
   
-        int minutesIndex = s.indexOf("minutes=");
-        int startQuoteIndex = s.indexOf('"', minutesIndex) + 1;
-        int endQuoteIndex = s.indexOf('"', startQuoteIndex);
-        String minutes = s.substring(startQuoteIndex, endQuoteIndex);
-        String output;
+  String query = String("/service/publicXMLFeed?command=predictions&a=sf-muni&r=" + 
+                            routeNum + 
+                            "&s=" + 
+                            stopNum);
 
-        if (minutes.length() == 1) {
-          output = String(routeIndex + "0" + minutes);
-        }
+  debug("Created request for URL: " + String(HTTP_URL) + query);
+  
+  return String("GET " + query + " HTTP/1.0");
+}
 
-        else if (minutes.length() > 2) {
-          output = String(routeIndex + "--" );
-        }
+void sendRequest(String routeNum, String stopNum) 
+{
+  // Send the HTTP request
+  
+  String request = createRequest(routeNum, stopNum);
 
-        else 
-        {
-          output = String(routeIndex + minutes);
-        }
-        
-        debug(output);
-        displayWrite(output, dot);
-        
-      } else {
-        debug("Didn't find prediction");
-        String output = String(routeIndex + "--");
-        
-        debug(output);
-        displayWrite(output, dot);
-      }  
+  debug("Making request");
+  debug(request);
+  
+  client.println(request);
+  client.println("Host: " + String(HTTP_URL));
+  client.println("User-Agent: ESP32");
+  client.println();
+}
+
+String getPrediction() {
+  // After a request is sent, try to find a prediction
+  // in the response. If it exists, the estimate will be
+  // in an XML tag which looks like this:
+  //    <prediction epochTime="1551081565747" seconds="1673" minutes="27" />
+  
+  String s;
+  
+  if(client.find("<prediction "))
+  {
+    
+    debug("Found prediction");
+    s = client.readStringUntil('>');
+
+    int minutesIndex = s.indexOf("minutes=");
+    int startQuoteIndex = s.indexOf('"', minutesIndex) + 1;
+    int endQuoteIndex = s.indexOf('"', startQuoteIndex);
+    String minutes = s.substring(startQuoteIndex, endQuoteIndex);
+    
+    if (minutes.length() == 1) {
+      // Pad with zeros if under 10
+      return String("0" + minutes);
+    }
+    else if (minutes.length() > 2) {
+      // if over 99, don't bother
+      return String("--");
+    }
+    else 
+    {
+      return minutes;
+    }
+    
+  } 
+  else 
+  {
+    debug("Couldn't find prediction");
+    return String("--");
+  }  
+}
+
+void displayPrediction(String routeNum, String prediction, bool dot)
+{
+  // Display the route number and prediction on the display. 
+  
+  String output = String(routeNum + prediction);
+  debug(output);
+  displayWrite(output, dot);
+}
+
+void loop() 
+{
+  // Main execution loop
+
+  // Loop through routes
+  for (int i = 0; i < NUM_ROUTES; i++) 
+  {
+    debug("Connecting to " + String(HTTP_URL)); 
+    
+    if (!client.connect(HTTP_URL, HTTP_PORT)) 
+    {
+      debug("Error connecting to " + String(HTTP_URL));
       
-      delay(DELAY_TIME);
-    }    
+      delay(NEXTBUS_RECONNECT_TIME);
+      return;
+    }
+    
+    debug("Connected to " + String(HTTP_URL));
+
+    String routeIndex = routesAndStops[i][0];
+    String stopIndex = routesAndStops[i][1];
+    String routeDir = routesAndStops[i][2];
+
+    sendRequest(routeIndex, stopIndex);
+    String prediction = getPrediction();
+    displayPrediction(routeIndex, prediction, routeDir == OUTBOUND);
+    
+    delay(DELAY_TIME);
+  }    
 }
